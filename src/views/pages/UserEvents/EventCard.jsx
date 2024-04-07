7324904afa021da91059709eadc7dabdd24f54eb
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2024-04-07 14:29:31 +0530

    event image preview

--- src/views/pages/UserEvents/EventCard.jsx
+++ src/views/pages/UserEvents/EventCard.jsx
@@ -1,43 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Image } from "antd";
 import { format } from "date-fns";
-
 const EventCard = ({ events }) => {
   return (
     <div className="row row-sm w-100 mt-4">
       {events.map((event, index) => (
         <div className="col-7 col-sm-5 col-md-4 col-lg-5 col-xl-4" key={index}>
           <div className="card card-file">
-          {/*   <div className="dropdown-file">
-              <Link to="#" className="dropdown-link" data-bs-toggle="dropdown">
-                <i className="fa fa-ellipsis-v" />
-              </Link>
-              <div className="dropdown-menu dropdown-menu-right">
-                <Link to="#" className="dropdown-item">
-                  View Details
-                </Link>
-                <Link to="#" className="dropdown-item">
-                  Share
-                </Link>
-                <Link to="#" className="dropdown-item">
-                  Download
-                </Link>
-                <Link to="#" className="dropdown-item">
-                  Rename
-                </Link>
-                <Link to="#" className="dropdown-item">
-                  Delete
-                </Link>
-              </div>
-            </div> */}
-            <div className="card-file-thumb">
-              <i className={`fa-regular `} />
+            <div>
+              <Image src={event?.media?.data?.attributes?.url} alt="image" />
             </div>
             <div className="card-body">
-              <h6>
-                <Link to="#">{event?.title}</Link>
-              </h6>
-          {/*     <span>{event?.desc}</span> */}
+              <h6>{event?.title}</h6>
             </div>
             <div className="card-footer">
               <div> Start : {format(new Date(event?.start), "dd/MM/yyyy")}</div>
