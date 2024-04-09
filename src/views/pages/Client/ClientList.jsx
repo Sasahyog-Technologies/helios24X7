40e9e59ffd5611e0532dd5cf3b357e9aa223982f
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2024-04-09 17:03:41 +0530

    ..

--- src/views/pages/Client/ClientList.jsx
+++ src/views/pages/Client/ClientList.jsx
@@ -1,8 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
 import { Table } from "antd";
 import { format } from "date-fns";
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import Breadcrumbs from "../../../components/Breadcrumbs";
 
 import ClientAddPopup from "../../../components/modelpopup/Client/ClientAddPopup";
