4311c9547158bd5af837e326c868ca8406b8586a
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2024-04-09 11:13:40 +0530

    responsive table in ptpList

--- src/views/pages/PTP/PTPList.jsx
+++ src/views/pages/PTP/PTPList.jsx
@@ -8,9 +8,11 @@ import request from "../../../sdk/functions";
 import PTPListFilter from "./PTPListFilter";
 import { render } from "@testing-library/react";
 import { FormatTime } from "../../../utils/timeFormater";
+import { useMediaQuery } from "usehooks-ts";
 
 const PTPList = () => {
   const [planId, setPlanId] = useState(null);
+  const isWebDevice = useMediaQuery("(min-width:700px)");
   const columns = [
     {
       title: "Trainee",
@@ -69,6 +71,80 @@ const PTPList = () => {
       ),
     }, */
   ];
+
+  /* --------------------------------------------------------------------------- */
+
+  const deviceColumns = [
+    {
+      render: (record, key, index) => {
+        return (
+          <div>
+            {/* <div className="d-flex justify-content-between">
+              {<div className="fw-bold fs-6"></div>}
+              <div
+                className="dropdown dropdown-action text-end" 
+              >
+                <Link
+                  to="#"
+                  className="action-icon dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <i className="material-icons">more_vert</i>
+                </Link>
+
+                <div className="dropdown-menu dropdown-menu-right">
+                  <Link
+                    className="dropdown-item"
+                    to="#"
+                    data-bs-toggle="modal"
+                    data-bs-target="#edit_event"
+                    onClick={() => seteventId(record.id)}
+                  >
+                    <i className="fa fa-pencil m-r-5" /> Edit
+                  </Link>
+                  <Link
+                    className="dropdown-item"
+                    to="#"
+                    data-bs-toggle="modal"
+                    data-bs-target="#delete_event"
+                    onClick={() => seteventId(record.id)}
+                  >
+                    <i className="fa fa-trash m-r-5" /> Delete
+                  </Link>
+                </div>
+              </div>
+            </div> */}
+
+            <div>
+              <div className="d-flex justify-content-between">
+                <span className="fw-bold fs-6">Trainee </span>
+                <span> {record?.trainee}</span>
+              </div>
+              <div className="d-flex justify-content-between">
+                <span className="fw-bold fs-6">Trainer </span>
+                <span> {record?.trainer}</span>
+              </div>
+              <div className="d-flex justify-content-between">
+                <span className="fw-bold fs-6">Session From </span>
+                <span>
+                  <span>{`${FormatTime(record?.session_from)}`}</span>
+                </span>
+              </div>
+              <div className="d-flex justify-content-between">
+                <span className="fw-bold fs-6">Session To </span>
+                <span>
+                  <span>{`${FormatTime(record?.session_to)}`}</span>
+                </span>
+              </div>
+            </div>
+          </div>
+        );
+      },
+    },
+  ];
+
+  /* --------------------------------------------------------------------------- */
 
   const [tableParams, setTableParams] = useState({
     pagination: {
@@ -183,7 +259,7 @@ const PTPList = () => {
                 <Table
                   loading={PTPIsLoading || isRefetching}
                   className="table-striped"
-                  columns={columns}
+                  columns={isWebDevice ? columns : deviceColumns}
                   dataSource={PTPData}
                   pagination={{
                     total: tableParams.pagination.total,
